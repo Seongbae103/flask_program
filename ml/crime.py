0117d9d8ac7f527befa8827594bc1df0f7bd42a7
Seongbae103 <seongbae103@gmail.com>
2022-11-17 18:23:51 +0900

    11-17

--- ml/crime.py
+++ ml/crime.py
@@ -1,62 +1,62 @@
 import googlemaps
+import numpy as np
 import pandas as pd
+import pickle
+from sklearn import preprocessing
+import folium
+
 '''
 주어진 데이터를 활용해서 서울시내 경찰서 범죄발생과 검거율 현황지도(폴리움)를 작성하시오
 '''
-CRIME_MENUS = ["종료", #0
-                "spec",#1
-                "Merge", #2
-                "interval",#3
-                "ratio",
-                "norminal",
+CRIME_MENUS = ["Exit", #0
+                "Show Spec",#1
+                "Save PolicePosition", #2
+                "Save Cctv Population",#3
+                "Save Police Normalization",
+                "Folium Example",
                 "ordinal",#4
                 "target",
                 "샘플링",#5
                 "모델링",#6
                 "학습",#7
                 "예측"]#8
-crime_meta = {
-    'id':'아이디', 'gender':'성별', 'age':'나이',
-    'hypertension':'고혈압',
-    'heart_disease':'심장병',
-    'ever_married':'기혼여부',
-    'work_type':'직종',
-    'Residence_type':'거주형태',
-    'avg_glucose_level':'평균혈당',
-    'bmi':'체질량지수',
-    'smoking_status':'흡연여부',
-    'stroke':'뇌졸중'
-}
+crime_meta = {}
 crime_menu = {
     "1" : lambda t: t.spec(),
     "2" : lambda t: t.save_police_pos(),
-    "3" : lambda t: t.interval(),
-    "4" : lambda t: t.ratio(),
-    "5" : lambda t: t.norminal(),
+    "3" : lambda t: t.save_cctv_pos(),
+    "4" : lambda t: t.save_police_norm(),
+    "5" : lambda t: t.folium_example(),
     "6" : lambda t: t.ordinal(),
     "7" : lambda t: t.target(),
     "8" : lambda t: t.partition(),
-    "9" : lambda t: t.popu(),
-
+    "9" : lambda t: print(),
 }
 class Crime:
     def __init__(self):
-        self.cctv = pd.read_csv('./data/cctv_in_seoul.csv')
         self.seoul_crime = pd.read_csv('./data/crime_in_seoul.csv')
-        self.population = pd.read_excel('./data/pop_in_seoul.xls', usecols=[1,3,6,9,13], skiprows=[0,2])
+        cols = ['절도 발생', '절도 검거', '폭력 발생', '폭력 검거']
+        self.seoul_crime[cols] = self.seoul_crime[cols].replace(',', '', regex=True).astype(int)  #, regex=True
+        self.cctv = pd.read_csv('data/cctv_in_seoul.csv')
+        self.pop = pd.read_excel('./data/pop_in_seoul.xls', names = ['자치구','합계','한국인','등록외국인','65세이상고령자'], usecols=[1,3,6,9,13], skiprows=[1,2])
 
+        self.crime_rate_columns = ['살인검거율', '강도검거율', '강간검거율', '절도검거율', '폭력검거율']
+        self.crime_columns = ['살인', '강도', '강간', '절도', '폭력']
+        self.arrest_columns = ['살인 검거', '강도 검거', '강간 검거', '절도 검거', '폭력 검거']
+        self.us_states = pd.read_json('./data/us-states.json')
+        self.us_unemployment = pd.read_csv('./data/us_unemployment.csv')
     def spec(self):
         pd.set_option('display.max_columns', None)
         pd.set_option('display.max_rows', None)
-        [(lambda x: print(f" --- 1.Shape ---\n{x.shape}"
-                               f"--- 2.Features ---\n{x.columns}"
-                               f"--- 3.Info ---\n{x.info()}"
-                               f"--- 4.Case Top1 ---\n{x.head(1)}"
-                               f"--- 5.Case Bottom1 ---\n{x.tail(3)}"
-                               f"--- 6.Describe ---{x.describe()}"
+        [(lambda x: print(f" --- 1.Shape ---\n{x.shape}\n"
+                               f"--- 2.Features ---\n{x.columns}\n"
+                               f"--- 3.Info ---\n{x.info()}\n"
+                               f"--- 4.Case Top1 ---\n{x.head(1)}\n"
+                               f"--- 5.Case Bottom1 ---\n{x.tail(3)}\n"
+                               f"--- 6.Describe ---{x.describe()}\n"
                                f"--- 7.Describe All ---\n{x.describe(include='all')}"))(i) for i in [self.seoul_crime, self.cctv]]
-        print(self.population)
-    def save_police_pos(self):
+        print(self.pop)
+    def save_police_pos(self): #norminal
         crime = self.seoul_crime
         station_names = []
         for name in crime['관서명']:
@@ -83,7 +83,124 @@ class Crime:
             gu_name = [gu for gu in _ if gu[-1]=='구'][0]
             gu_names.append(gu_name)
         crime['구별'] = gu_names
-        crime.to_csv('./save/police_pos.csv', index=False)
+        # 구와 경찰서의 위치가 다른 경우 수작업
+        crime.loc[crime['관서명'] == '혜화서', ['구별']] == '종로구'
+        crime.loc[crime['관서명'] == '서부서', ['구별']] == '은평구'
+        crime.loc[crime['관서명'] == '강서서', ['구별']] == '양천구'
+        crime.loc[crime['관서명'] == '종암서', ['구별']] == '성북구'
+        crime.loc[crime['관서명'] == '방배서', ['구별']] == '서초구'
+        crime.loc[crime['관서명'] == '수서서', ['구별']] == '강남구'
+        crime.to_pickle('./save/police_pos.pkl')
+        print(pd.read_pickle('./save/police_pos.pkl'))
+
+    def save_cctv_pos(self): #ratio로 판단 -> norminal로 저장
+        cctv = self.cctv
+        pop = self.pop
+        cctv.rename(columns={cctv.columns[0]:'구별'}, inplace=True)
+        pop.rename(columns={
+            pop.columns[0]:'구별',
+            pop.columns[1]:'인구수',
+            pop.columns[2]:'한국인',
+            pop.columns[3]:'외국인',
+            pop.columns[4]:'고령자'
+        }, inplace=True)
+        print(pop)
+        print("*"*100)
+        pop.drop(index=26, inplace=True)
+
+        pop['외국인비율'] =pop['외국인'].astype(int)/pop['인구수'].astype(int) *100
+        pop['고령자비율'] =pop['고령자'].astype(int)/pop['인구수'].astype(int) *100
+        cctv.drop(["2013년도 이전","2014년","2015년","2016년"], axis=1, inplace=True)
+        cctv_pop = pd.merge(cctv, pop, on='구별')
+        cor1 = np.corrcoef(cctv_pop['고령자비율'], cctv_pop['소계'])
+        cor2 = np.corrcoef(cctv_pop['외국인비율'], cctv_pop['소계'])
+        print(f'고령자비율과 CCTV의 상관계수 {str(cor1)} \n'
+              f'외국인비율과 CCTV의 상관계수 {str(cor2)} ')
+        """
+         고령자비율과 CCTV 의 상관계수 [[ 1.         -0.28078554]
+                                     [-0.28078554  1.        ]] 
+         외국인비율과 CCTV 의 상관계수 [[ 1.         -0.13607433]
+                                     [-0.13607433  1.        ]]
+        r이 -1.0과 -0.7 사이이면, 강한 음적 선형관계,
+        r이 -0.7과 -0.3 사이이면, 뚜렷한 음적 선형관계,
+        r이 -0.3과 -0.1 사이이면, 약한 음적 선형관계,
+        r이 -0.1과 +0.1 사이이면, 거의 무시될 수 있는 선형관계,
+        r이 +0.1과 +0.3 사이이면, 약한 양적 선형관계,
+        r이 +0.3과 +0.7 사이이면, 뚜렷한 양적 선형관계,
+        r이 +0.7과 +1.0 사이이면, 강한 양적 선형관계
+        고령자비율 과 CCTV 상관계수 [[ 1.         -0.28078554] 약한 음적 선형관계
+                                    [-0.28078554  1.        ]]
+        외국인비율 과 CCTV 상관계수 [[ 1.         -0.13607433] 거의 무시될 수 있는
+                                    [-0.13607433  1.        ]]                        
+         """
+        cctv_pop.to_pickle('./save/cctv_pop.pkl')
+        print(pd.read_pickle('./save/cctv_pop.pkl'))
+
+    def save_police_norm(self):
+        police_pos = pd.read_pickle('./save/police_pos.pkl')
+        police = pd.pivot_table(police_pos, index="구별", aggfunc=np.sum)
+        police['살인검거율'] = (police['살인 검거'] / police['살인 발생']) * 100
+        police['강도검거율'] = (police['강도 검거'] / police['강도 발생']) * 100
+        police['강간검거율'] = (police['강간 검거'] / police['강간 발생']) * 100
+        police['절도검거율'] = (police['절도 검거'] / police['절도 발생']) * 100
+        police['폭력검거율'] = (police['폭력 검거'] / police['폭력 발생']) * 100
+        police.drop(columns={'살인 검거','강도 검거','강간 검거','절도 검거','폭력 검거'}, axis=1, inplace=True)
+        print(police)
+        for i in self.crime_rate_columns:
+            police.loc[police[i]>100, 1] = 100 # 데이터값의 기간 오류로 100을 넘으면 100으로 계산
+        police.rename(columns={
+            '살인 발생': '살인',
+            '강도 발생': '강도',
+            '강간 발생': '강간',
+            '절도 발생': '절도',
+            '폭력 발생': '폭력'
+        }, inplace=True)
+        x = police[self.crime_rate_columns].values
+        min_max_scalar = preprocessing.MinMaxScaler()
+        """
+        스케일링은 선형변환을 적용하여
+        전체 자료의 분포를 평균 0, 분산 1이 되도록 만드는 과정
+        """
+        x_scaled = min_max_scalar.fit_transform(x.astype(float))
+        """
+        정규화 normalization
+        많은 양의 데이터를 처리함에 있어 데이터의 범위(도메인)를 일치시키거나
+        분포(스케일)를 유사하게 만드는 작업
+        """
+        police_norm = pd.DataFrame(x_scaled, columns=self.crime_columns, index=police.index)
+        print(police_norm)
+        police_norm[self.crime_rate_columns]= police[self.crime_rate_columns]
+        police_norm['범죄']= np.sum(police[self.crime_rate_columns])
+        police_norm['검거']= np.sum(police[self.crime_columns], axis=1)
+        police_norm.to_pickle('./save/police_norm.pkl')
+        print(police_norm.to_pickle('./save/police_norm.pkl'))
+
+    def folium_example(self):
+        us_states = self.us_states
+        us_unemployment = self.us_unemployment
+
+        url = (
+            "https://raw.githubusercontent.com/python-visualization/folium/master/examples/data"
+        )
+        state_geo = f"{url}/us-states.json"
+        state_unemployment = f"{url}/US_Unemployment_Oct2012.csv"
+        state_data = pd.read_csv(state_unemployment)
+
+        bins = list(us_unemployment["Unemployment"].quantile([0, 0.25, 0.5, 0.75, 1]))
+        m = folium.Map(location=[48, -102], zoom_start=5)
+        folium.Choropleth(
+            geo_data=state_geo,  # us_states,
+            data=state_data,  # us_unemployment,
+            name="choropleth",
+            columns=["State", "Unemployment"],
+            key_on="feature.id",
+            fill_color="YlGn",
+            fill_opacity=0.7,
+            line_opacity=0.5,
+            legend_name='Unemployment Rate (%)',
+            bins=bins
+        ).add_to(m)
+        m.save("./save/unemployment.html")
 
 
     def interval(self):
@@ -91,10 +208,7 @@ class Crime:
         inter = ['소계','2013년도 이전','2014년','2015년','2016년']
         print(f'--- 구간변수 기초 통계량 --- \n{t[inter].describe()}')
 
-    def ratio(self):
-        pass
-    def norminal(self):
-        pass
+
     def ordinal(self):
         pass
     def target(self):
