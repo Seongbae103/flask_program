f8fcea7e85f279666f79410c0689d93c4f872c66
Seongbae103 <seongbae103@gmail.com>
2022-11-18 17:42:03 +0900

    11-18

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    a = {}
-    return "Hello Flask!!"
+    return "Hollo World !!"
+
+@app.route("/users/<username>")
+def get_user(username):
+    return username
+
+@app.route("/posts/<int:post_id>")
+def get_post(post_id):
+    return str(post_id)
+
+@app.route("/uuid/<uuid:uuid>")
+def uuid(uuid):
+    return str(uuid)
+
+@app.route("/login", methods=["GET", "POST"])
+def login_page():
+    if request.method == 'POST':
+        return "로그인 성공"
+    else: return "로그인 화면"
 
 if __name__ =="__main__":
     app.run()
